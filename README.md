f3a6f3e8dd17d285518b31e7e1faa714824b7168
heiwa4126 <heiwa4126@gmail.com>
2021-09-27 11:37:27 +0900

    initial

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # goembed_ex_fs
 
 go 1.16から追加された機能
-`go:embed`の最小のサンプルコード2。
+`go:embed`の最小のサンプルコード2。embed.FS版。
 
 参照: [embed package - embed - pkg.go.dev](https://pkg.go.dev/embed)
 
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	_ "embed"
+	"embed"
+	"log"
 )
 
-//go:embed static/hello.txt
-var m1 string
+//go:embed static other_static
+var f embed.FS
 
-//go:embed static/goodbye.txt
-var m2 string
+// 複数のディレクトリをまとめられる。他特定のextentionのファイルだけ、とかも出来る。
+// https://pkg.go.dev/embed の例参照。
+
+func readFromEmbedFS(file string) string {
+	data, err := f.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(data)
+}
 
 func main() {
-	print(m1)
-	print(m2)
+	print(readFromEmbedFS("static/hello.txt"))
+	print(readFromEmbedFS("other_static/goodbye.txt"))
 }
